c82fca1aa1d13b1c5fd5a63de40043e5c443c7c3
Alexsandr <venoman9867@gmail.com>
2022-09-04 17:02:29 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите значения через пробел");
+string[] s=Console.ReadLine().Split(" ");
+foreach(string value in s){
+    if(value.Length<=3){
+        Console.WriteLine(value);
+    }
+}
